bcdbd17d5ff4e5a5b046dc80ebeae19a229af31c
TheDude <david@vsodir.onmicrosoft.com>
2022-03-24 14:52:22 +0000

    Fix after merge master onto local branch

--- src/Blockcore.Indexer.Cirrus/Storage/Mongo/SmartContracts/IDAOContractAggregator.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using Blockcore.Indexer.Cirrus.Storage.Mongo.Types;
-
-namespace Blockcore.Indexer.Cirrus.Storage.Mongo.SmartContracts;
-
-public interface IDAOContractAggregator
-{
-   Task<DaoContractComputedTable> ComputeDaoContractForAddressAsync(string address);
-}
