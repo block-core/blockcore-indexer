4a062394ae5aee2c77fe036b302a846b69832b1f
TheDude <david@vsodir.onmicrosoft.com>
2022-03-28 14:37:44 +0100

    Added support for extracting the token URI from the store on the mint log reader

--- src/Blockcore.Indexer.Cirrus/Storage/Mongo/SmartContracts/NonFungibleToken/MintLogReader.cs
+++ src/Blockcore.Indexer.Cirrus/Storage/Mongo/SmartContracts/NonFungibleToken/MintLogReader.cs
@@ -9,21 +9,22 @@ public class MintLogReader : ILogReader<NonFungibleTokenComputedTable>
 {
    public bool CanReadLogForMethodType(string methodType) => methodType.Equals("Mint") || methodType.Equals("SafeMint");
 
-   public bool IsTransactionLogComplete(LogResponse[] logs) => true;
+   public bool IsTransactionLogComplete(LogResponse[] logs) => false;
 
    public void UpdateContractFromTransactionLog(CirrusContractTable contractTransaction,
       NonFungibleTokenComputedTable computedTable)
    {
-      var log = contractTransaction.Logs.SingleOrDefault()?.Log;
+      var log = contractTransaction.Logs.First()?.Log;
+      var uriLog = contractTransaction.Logs.Last()?.Log;
 
-      if (log is null)
+      if (log is null || uriLog is null)
          throw new ArgumentNullException(nameof(log));
 
       computedTable.Tokens.Add(new Token
       {
          Address = (string)log.Data["to"],
          Id = (long)log.Data["tokenId"],
-         Uri = "Todo get this from the store on the node api - use URI:TokeiId"
+         Uri = (string)uriLog.Data["tokenUri"]
       });
    }
 }
