62d19e1287ba8474f9cb2836a0ee978e64cd0195
dangershony <dan.gershony@gmail.com>
2022-03-28 18:08:41 +0100

    Create grouped lists of contract api calls

---
+++ src/Blockcore.Indexer.Cirrus/Models/QueryContractGroup.cs
@@ -0,0 +1,12 @@
+namespace Blockcore.Indexer.Cirrus.Models
+{
+   public class QueryContractGroup
+   {
+      public string ContractCodeType { get; set; }
+      
+      public int Count { get; set; }
+
+
+      public string ContractHash { get; set; }
+   }
+}
