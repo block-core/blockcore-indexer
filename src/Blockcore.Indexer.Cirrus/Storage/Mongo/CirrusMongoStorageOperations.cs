bd102d85d79d0ec2be80d01d771819cf1fbb59ae
dangershony <dan.gershony@gmail.com>
2022-04-28 19:33:05 +0100

    Prepare for handling of internal transfer

--- src/Blockcore.Indexer.Cirrus/Storage/Mongo/CirrusMongoStorageOperations.cs
+++ src/Blockcore.Indexer.Cirrus/Storage/Mongo/CirrusMongoStorageOperations.cs
@@ -61,6 +61,12 @@ namespace Blockcore.Indexer.Cirrus.Storage.Mongo
 
          foreach (Transaction transaction in item.Transactions)
          {
+            TxOut smartContractInternalCallTxOut = transaction.Outputs.FirstOrDefault(txOut => txOut.ScriptPubKey.IsSmartContractInternalCall());
+            if (smartContractInternalCallTxOut != null)
+            {
+               // handle sc internal transfer
+            }
+
             TxOut smartContractTxOut = transaction.Outputs.FirstOrDefault(txOut => txOut.ScriptPubKey.IsSmartContractExec());
 
             if (smartContractTxOut != null)
