5d1d38b672be6235e62e39ed4b45e99e15f50b63
YakupIpek <yakupipek@gmail.com>
2022-11-21 20:26:05 +0300

    Pagination Fix (#171)
    * Pagination Fix
    
    * Update CirrusMongoData.cs
    
    * Update CirrusMongoData.cs

--- src/Blockcore.Indexer.Cirrus/Storage/Mongo/CirrusMongoData.cs
+++ src/Blockcore.Indexer.Cirrus/Storage/Mongo/CirrusMongoData.cs
@@ -115,11 +115,11 @@ namespace Blockcore.Indexer.Cirrus.Storage.Mongo
       {
          var total = mongoDb.CirrusContractTable
             .AsQueryable()
-            .Count(_ => _.BlockIndex >= startBlock && _.BlockIndex < endBlock);
+            .Count(_ => _.BlockIndex >= startBlock && _.BlockIndex <= endBlock);
 
          var contracts = mongoDb.CirrusContractTable.AsQueryable()
-            .Where(_ => _.BlockIndex >= startBlock && _.BlockIndex < endBlock)
-            .Skip(offset ?? 0 * limit)
+            .Where(_ => _.BlockIndex >= startBlock && _.BlockIndex <= endBlock)
+            .Skip((offset ?? 0) * limit)
             .Take(limit)
             .ToList();
 
