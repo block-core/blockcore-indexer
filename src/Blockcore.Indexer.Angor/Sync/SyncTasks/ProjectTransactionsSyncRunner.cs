ded0b36cda4dd2bb59ded78ee8151335c0517705
TheDude <david@vsodir.onmicrosoft.com>
2023-09-19 12:46:21 +0100

    Added the lookup and insert of investments for a project

--- src/Blockcore.Indexer.Angor/Sync/SyncTasks/ProjectTransactionsSyncRunner.cs
+++ src/Blockcore.Indexer.Angor/Sync/SyncTasks/ProjectTransactionsSyncRunner.cs
@@ -1,10 +1,10 @@
+using Blockcore.Consensus.ScriptInfo;
 using Blockcore.Indexer.Angor.Storage.Mongo;
 using Blockcore.Indexer.Angor.Storage.Mongo.Types;
 using Blockcore.Indexer.Core.Settings;
-using Blockcore.Indexer.Core.Storage.Mongo.Types;
 using Blockcore.Indexer.Core.Sync.SyncTasks;
+using Blockcore.NBitcoin.DataEncoders;
 using Microsoft.Extensions.Options;
-using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 
@@ -26,25 +26,75 @@ public class ProjectTransactionsSyncRunner : TaskRunner
 
    public override async Task<bool> OnExecute()
    {
-      var testTrx = await angorMongoDb.ProjectTable.AsQueryable()
+      var investmentsInProjectOutputs = await angorMongoDb.ProjectTable.AsQueryable()
          .GroupJoin(
-            angorMongoDb.OutputTable.AsQueryable(),
-            p => p.AngorKey,
+            angorMongoDb.OutputTable.AsQueryable()
+               .AsQueryable(),
+            p => p.AddressOnFeeOutput,
             o => o.Address,
-            (project, outputs) => new { project, outputs })
-         .Where(_ => _.outputs.Count() > 3)
-         .SelectMany(projection => projection.outputs,
-            (p, o) => new Investment
-            {
-               AngorKey = p.project.AngorKey,
-               AmountSats = p.outputs.Where(_ => _.Address != p.project.AngorKey).Sum(_ => _.Value),
-               BlockIndex = o.BlockIndex,
-               SecretHash = o.ScriptHex.Substring(32),
-               TransactionIndex = o.Outpoint.TransactionId,
-               InvestorPubKey = o.ScriptHex.Substring(0, 32)
-            })
+            (project, outputs) => new { project.TransactionId, outputs })
+         .SelectMany(project => project.outputs,(p, o) => new
+         {
+            outputTransactionId = o.Outpoint.TransactionId,
+            isCreateProject = p.TransactionId == o.Outpoint.TransactionId,
+
+         })
+         .Where(t => t.isCreateProject == false)
+         .GroupJoin(angorMongoDb.InvestmentTable.AsQueryable(),
+            x => x.outputTransactionId,
+            i => i.TransactionIndex,
+            (data,investments) => new {data.outputTransactionId,data.isCreateProject,investments})
+         .Where(data => !data.investments.Any())
          .ToListAsync();
 
+      var investments = new List<Investment>();
+
+      foreach (var investmentOutput in investmentsInProjectOutputs)
+      {
+         var allOutputsOnInvestmentTransaction = await angorMongoDb.OutputTable.AsQueryable()
+            .Where(output => output.Outpoint.TransactionId == investmentOutput.outputTransactionId)
+            .ToListAsync();
+
+         if (allOutputsOnInvestmentTransaction.All(_ => _.Address != "none")) //TODO replace with a better indicator of stage investments
+            continue;
+
+         var feeOutput = allOutputsOnInvestmentTransaction.Single(_ => _.Outpoint.OutputIndex == 0);
+         var projectDataOutput = allOutputsOnInvestmentTransaction.Single(_ => _.Outpoint.OutputIndex == 1);
+
+         var projectInfoScript = Script.FromHex(projectDataOutput.ScriptHex);
+
+         var investorPubKey = Encoders.Hex.EncodeData(projectInfoScript.ToOps()[1].PushData);
+
+         if (investments.Any(_ => _.InvestorPubKey == investorPubKey) ||
+             angorMongoDb.InvestmentTable.AsQueryable().Any(_ => _.InvestorPubKey == investorPubKey)) //Investor key is the _id of that document
+         {
+            logger.LogInformation($"Multiple transactions with the same investor public key {investorPubKey} for the same project {feeOutput.ScriptHex}");
+            continue;
+         }
+
+         var hashOfSecret = projectInfoScript.ToOps().Count == 3
+            ? Encoders.Hex.EncodeData(projectInfoScript.ToOps()[2].PushData)
+            : string.Empty;
+
+         var investment = new Investment
+         {
+            InvestorPubKey = investorPubKey,
+            AngorKey = feeOutput.ScriptHex,
+            AmountSats = allOutputsOnInvestmentTransaction.Where(_ => _.Address == "none").Sum(_ => _.Value),
+            BlockIndex = feeOutput.BlockIndex,
+            SecretHash = hashOfSecret,
+            TransactionIndex = feeOutput.Outpoint.TransactionId,
+         };
+
+         investments.Add(investment);
+      }
+
+      if (!investments.Any())
+         return false;
+
+      await angorMongoDb.InvestmentTable.InsertManyAsync(investments);
+
       return true;
+
    }
 }
