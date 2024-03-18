fc325c4567a227dbd0cbb254f201b00444d48cb9
TheDude <davidgershony@gmail.com>
2024-03-18 17:16:27 +0000

    Added information to the stats endoint

--- src/Blockcore.Indexer.Angor/Operations/Types/ProjectStats.cs
+++ src/Blockcore.Indexer.Angor/Operations/Types/ProjectStats.cs
@@ -4,6 +4,7 @@ public class ProjectStats
 {
    public long InvestorCount { get; set; }
    public long AmountInvested { get; set; }
+   public long AmountSpentSoFarByFounder { get; set; }
    public long AmountInPenalties { get; set; }
-   public long CountInPenalties { get; set; }
+   public int CountInPenalties { get; set; }
 }
--- src/Blockcore.Indexer.Angor/Storage/Mongo/AngorMongoData.cs
+++ src/Blockcore.Indexer.Angor/Storage/Mongo/AngorMongoData.cs
@@ -9,6 +9,7 @@ using Blockcore.Indexer.Core.Storage.Mongo;
 using Blockcore.Indexer.Core.Storage.Types;
 using Blockcore.Indexer.Core.Sync;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Blockcore.Indexer.Angor.Storage.Mongo;
@@ -66,14 +67,147 @@ public class AngorMongoData : MongoData, IAngorStorage
             .Where(_ => _.AngorKey == projectId)
             .Sum(s => s.AmountSats);
 
+         // var totalInvestmentWithdrawnByInvestors = await GetTotalInvestmentWithdrawn(projectId);
+         //
+         // var totalOutputsUsed = await GetSumOfOutputsSpentOnProject(projectId);
+
+         var outputsSpentSummery = await GetSpentProjectFundsSplitToFounderAndPenalty(projectId);
+
+         var founder = outputsSpentSummery.SingleOrDefault(x => x["_id"] == "founder");
+
+         var investor = outputsSpentSummery.SingleOrDefault(x => x["_id"] == "investor");
+
          return new ProjectStats
          {
             InvestorCount = total,
             AmountInvested = sum,
+            AmountSpentSoFarByFounder = founder?["total"].AsInt64 ?? 0,
+            AmountInPenalties = investor?["total"].AsInt64 ?? 0,
+            CountInPenalties = investor?["trxCount"].AsInt32 ?? 0
          };
       }
 
       return null;
+   }
+
+
+   private Task<List<BsonDocument>> GetSpentProjectFundsSplitToFounderAndPenalty(string projectId)
+   {
+      return mongoDb.InvestmentTable.Aggregate(PipelineDefinition<Investment, BsonDocument>.Create(
+            new BsonDocument("$match",
+               new BsonDocument("AngorKey", projectId)),
+            new BsonDocument("$lookup",
+               new BsonDocument
+               {
+                  { "from", "Input" },
+                  { "localField", "TransactionId" },
+                  { "foreignField", "Outpoint.TransactionId" },
+                  { "as", "inputs" }
+               }),
+            new BsonDocument("$unwind", "$inputs"),
+            new BsonDocument("$match",
+               new BsonDocument("inputs.Address", "none")), //Remove change address
+            new BsonDocument("$group",
+               new BsonDocument
+               {
+                  { "_id",
+                     new BsonDocument
+                     {
+                        { "TransactionId", "$TransactionId" },
+                        { "TrxHash", "$inputs.TrxHash" }
+                     } },
+                  { "spent",
+                     new BsonDocument("$sum", "$inputs.Value") },
+                  { "numTrx",
+                     new BsonDocument("$sum", 1) }
+               }),
+            new BsonDocument("$group",
+               new BsonDocument
+               {
+                  { "_id",
+                     new BsonDocument("$cond",
+                        new BsonDocument
+                        {
+                           { "if",
+                              new BsonDocument("$eq",
+                                 new BsonArray
+                                 {
+                                    "$numTrx",
+                                    1
+                                 }) },
+                           { "then", "founder" },
+                           { "else", "investor" }
+                        }) },
+                  { "total",
+                     new BsonDocument("$sum", "$spent") },
+                  { "trxCount",
+                     new BsonDocument("$sum", 1) }
+               })))
+         .ToListAsync();
+   }
+
+   private Task<BsonDocument> GetTotalInvestmentWithdrawn(string projectId)
+   {
+      return mongoDb.InvestmentTable.Aggregate(PipelineDefinition<Investment, BsonDocument>.Create(new[]
+         {
+            new BsonDocument("$match",
+               new BsonDocument("AngorKey", projectId)),
+            new BsonDocument("$lookup",
+               new BsonDocument
+               {
+                  { "from", "Input" },
+                  { "localField", "TransactionId" },
+                  { "foreignField", "Outpoint.TransactionId" },
+                  { "as", "joinedData" }
+               }),
+            new BsonDocument("$unwind", "$joinedData"),
+            new BsonDocument("$group",
+               new BsonDocument
+               {
+                  {
+                     "_id", new BsonDocument { { "AngorKey", "$AngorKey" }, { "TransactionId", "$joinedData.TrxHash" } }
+                  },
+                  { "count", new BsonDocument("$sum", 1) },
+                  { "totalValue", new BsonDocument("$sum", "$joinedData.Value") }
+               }),
+            new BsonDocument("$match",
+               new BsonDocument("count",
+                  new BsonDocument("$gt", 1))),
+            new BsonDocument("$group",
+               new BsonDocument
+               {
+                  { "_id", "$_id.AngorKey" }, { "totalValueSum", new BsonDocument("$sum", "$totalValue") }
+               }),
+            new BsonDocument("$project",
+               new BsonDocument { { "_id", 0 }, { "AngorKey", "$_id" }, { "totalValueSum", 1 } })
+         }))
+         .FirstOrDefaultAsync();
+   }
+
+   private Task<BsonDocument> GetSumOfOutputsSpentOnProject(string projectId)
+   {
+      return mongoDb.InvestmentTable.Aggregate(PipelineDefinition<Investment, BsonDocument>.Create(new[]
+      {
+         new BsonDocument("$match",
+            new BsonDocument("AngorKey", projectId)),
+         new BsonDocument("$lookup",
+               new BsonDocument
+               {
+                  { "from", "Input" },
+                  { "localField", "TransactionId" },
+                  { "foreignField", "Outpoint.TransactionId" },
+                  { "as", "inputs" }
+               }),
+            new BsonDocument("$unwind",
+               new BsonDocument("path", "$inputs")),
+            new BsonDocument("$group",
+               new BsonDocument
+               {
+                  { "_id", "$AngorKey" },
+                  { "Spent",
+                     new BsonDocument("$sum", "$inputs.Value") }
+               })
+         })).FirstOrDefaultAsync();
    }
 
    public async Task<QueryResult<ProjectIndexerData>> GetProjectsAsync(int? offset, int limit)
